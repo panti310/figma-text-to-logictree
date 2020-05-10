015479eb76ef50b773b6ddca954cd1eed7d453c8
Masahiro Sato <3ra8.2ru310.3da.1za.0ka8ma2@gmail.com>
2020-05-10 11:40:49 +0900

    generalized dictionary creation process

--- src/code.ts
+++ src/code.ts
@@ -1,12 +1,14 @@
-const targetText: TextNode = getTextFromArray();
+const inputText: TextNode = getTextFromSelection();
+const inputDict: { [key: string]: {} } = {};
+let processingKeyArray: Array<string> = [];
 
-if (targetText != null) {
-  createDictonary(targetText.characters);
+if (inputText != null) {
+  convertTextIntoDict(inputText.characters);
 }
 
 figma.closePlugin();
 
-function getTextFromArray() {
+function getTextFromSelection() {
   const selectionArray = figma.currentPage.selection;
   if (selectionArray === null) {
     return null;
@@ -21,37 +23,84 @@ function getTextFromArray() {
   return null;
 }
 
-function createDictonary(chara: string) {
-  const dict: { [key: string]: {} } = {};
+
+function convertTextIntoDict(chara: string) {
   const charaSplitArray: Array<string> = chara.split('\n');
-  let currentKeysArray: Array<string> = [];
   
   charaSplitArray.forEach(line => {
-    const lineSplitArray = line.split('- ');
-    console.log(lineSplitArray);
-    const indent = Number(lineSplitArray[0].length) / 2;
-    const key = lineSplitArray[1];
-
-    if (indent === 0) {
-      currentKeysArray = [key];
-      dict[key] = {};
-    } else if (indent === 1) {
-      if (currentKeysArray.length > indent) {
-        currentKeysArray = currentKeysArray.splice(indent - 1, 1);
-      }
-      currentKeysArray.push(key);
-      const targetDict = dict[currentKeysArray[0]];
-      targetDict[key] = {};
-      dict[currentKeysArray[0]] = targetDict;
-    } else if (indent === 2) {
-      if (currentKeysArray.length > indent) {
-        currentKeysArray = currentKeysArray.splice(indent - 1, 1);
-      }
-      const targetDict = dict[currentKeysArray[0]][currentKeysArray[1]];
-      targetDict[key] = {};
-      dict[currentKeysArray[0]][currentKeysArray[1]] = targetDict;
-    }
-    console.log("currentKeysArray: " + currentKeysArray);
+    addToDict(line);
   });
-  console.log(dict);
+  console.log(inputDict);
+}
+
+function addToDict(line: string) {
+  const lineSplitArray = line.split('- ');
+  const indent = Number(lineSplitArray[0].length) / 2;
+  const key = lineSplitArray[1];
+
+  if (indent === 0) {
+    processingKeyArray = [key];
+    inputDict[key] = {};
+  }
+  else {
+    deleteRedundantElem(indent);
+    addKeyToDict(key, indent);
+  }
+  console.log("processingKeyArray: " + processingKeyArray);
+}
+
+function deleteRedundantElem(indent: number) {
+  if (processingKeyArray.length > indent) {
+    processingKeyArray = processingKeyArray.splice(0, indent);
+  }
+}
+
+function addKeyToDict(key: string, indent: number) {
+  let processingDict: { [key: string]: {} } = {};
+  for (let i = 0; i < processingKeyArray.length; i++) {
+    if (i === 0) {
+      processingDict = inputDict[processingKeyArray[i]];
+    } else {
+      processingDict = processingDict[processingKeyArray[i]];
+    }
+  }
+  processingKeyArray.push(key);
+  processingDict[key] = {};
+
+  addProcessingDictToDict(indent, processingDict);
+}
+
+function addProcessingDictToDict(indent: number, targetDict: { [key: string]: {}; }) {
+  switch (indent) {
+    case 1:
+      inputDict[processingKeyArray[0]] = targetDict;
+      break;
+    case 2:
+      inputDict[processingKeyArray[0]][processingKeyArray[1]] = targetDict;
+      break;
+    case 3:
+      inputDict[processingKeyArray[0]][processingKeyArray[1]][processingKeyArray[2]] = targetDict;
+      break;
+    case 4:
+      inputDict[processingKeyArray[0]][processingKeyArray[1]][processingKeyArray[2]][processingKeyArray[3]] = targetDict;
+      break;
+    case 5:
+      inputDict[processingKeyArray[0]][processingKeyArray[1]][processingKeyArray[2]][processingKeyArray[3]][processingKeyArray[4]] = targetDict;
+      break;
+    case 6:
+      inputDict[processingKeyArray[0]][processingKeyArray[1]][processingKeyArray[2]][processingKeyArray[3]][processingKeyArray[4]][processingKeyArray[5]] = targetDict;
+      break;
+    case 7:
+      inputDict[processingKeyArray[0]][processingKeyArray[1]][processingKeyArray[2]][processingKeyArray[3]][processingKeyArray[4]][processingKeyArray[5]][processingKeyArray[6]] = targetDict;
+      break;
+    case 8:
+      inputDict[processingKeyArray[0]][processingKeyArray[1]][processingKeyArray[2]][processingKeyArray[3]][processingKeyArray[4]][processingKeyArray[5]][processingKeyArray[6]][processingKeyArray[7]] = targetDict;
+      break;
+    case 9:
+      inputDict[processingKeyArray[0]][processingKeyArray[1]][processingKeyArray[2]][processingKeyArray[3]][processingKeyArray[4]][processingKeyArray[5]][processingKeyArray[6]][processingKeyArray[7]][processingKeyArray[8]] = targetDict;
+      break;
+    default:
+      console.log('階層が深すぎて対処できません');
+      break;
+  }
 }
